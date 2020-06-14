3c7524b468f9de05d7b9edb8ef132db2d25cbe90
maheenrao <maheenrao@users.noreply.github.com>
2020-06-14 13:14:00 +0500

    push

---
+++ src/context/GlobalState.js
@@ -0,0 +1,31 @@
+import React, { createContext, useReducer, children} from 'react';
+import AppReducer from './AppReducer';
+const initialState= {
+    transactions: []
+}
+export const GlobalContext = createContext (initialState);
+export const GlobalProvider=({Children})=> {
+   const [state, dispatch]= useReducer (AppReducer,initialState); 
+
+   function deleteTransaction(id){
+       dispatch({
+           type: 'DELETE_TRANSACTION',
+           payload: id
+       });
+   }
+   function addTransaction(transactions){
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transactions
+    });
+}
+
+   return (<GlobalContext.Provider value={{
+       transactions:state.transactions,
+       deleteTransaction,
+       addTransaction
+   }}>
+    {children}
+    </GlobalContext.Provider>);
+
+}
